08c0ddebd2945e299ce2460a144fb6c03e6df78e
Arjun Viswaa <arjunviswaa@gmail.com>
2025-07-28 14:46:09 +0530

    Node JS Templating engines

--- Projects/Express_Bsc/routes/admin.js
+++ Projects/Express_Bsc/routes/admin.js
@@ -1,24 +1,29 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
-const rootDir = require("../util/path");
+
 const products = [];
 
-// -> /admin/add-product - GET
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" /><button type="submit">Submit</button></form>');
-    res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
-// -> /admin/add-product - POST
-router.post('/add-product', (req, res) => {
-    products.push({
-        title: req.body.title
-    });
-    res.redirect("/");
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
-module.exports = {
-    router,
-    products
-}
+exports.routes = router;
+exports.products = products;
