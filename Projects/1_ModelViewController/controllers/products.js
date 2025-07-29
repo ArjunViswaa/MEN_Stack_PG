64076d8c9bb9e4a258ad9082aca47d5633c1bde5
Arjun Viswanath <arjun.viswanath@rentokil-initial.com>
2025-07-29 19:28:12 +0530

    Node JS Practice shopping app

--- Projects/1_ModelViewController/controllers/products.js
+++ Projects/1_ModelViewController/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 
 const getAddProducts = (req, res, next) => {
-  res.render('add-product', {
+  res.render('admin/add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     formsCSS: true,
@@ -18,7 +18,7 @@ const postAddProducts = (req, res, next) => {
 
 const getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
-    res.render('shop', {
+    res.render('shop/products-list', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
