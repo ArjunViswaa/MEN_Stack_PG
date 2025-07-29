6793bbc2ed2c5d7631b1bb4461df8073925ae473
Arjun Viswaa <arjunviswaa@gmail.com>
2025-07-29 07:37:35 +0530

    Node JS MVC Final

--- Projects/1_ModelViewController/controllers/products.js
+++ Projects/1_ModelViewController/controllers/products.js
@@ -1,33 +1,36 @@
-const products = [];
+const Product = require("../models/product");
 
 const getAddProducts = (req, res, next) => {
-    res.render('add-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
-    });
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 };
 
 const postAddProducts = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const newProduct = new Product(req.body.title);
+  newProduct.save();
   res.redirect('/');
 };
 
 const getProducts = (req, res, next) => {
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  Product.fetchAll((products) => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
   });
 };
 
 module.exports = {
-    getAddProducts,
-    postAddProducts,
-    getProducts
+  getAddProducts,
+  postAddProducts,
+  getProducts
 };
